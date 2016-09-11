4ca88608279939de78d2e931945f81fbc66567c9
Benjamin M6 <benjamynfrank101@gmail.com>
2016-09-11 13:12:16 +0100

    Add a python script that renames files downloaded with Operamini containing the %20 character to have spaces instead

---
+++ RenameFile.py
@@ -0,0 +1,22 @@
+import os
+"""Renames files in a directory
+(1)deletes the %20 appended to it
+(2)replaces it with space
+
+Usage:
+python RenameFile.py
+put in directory you wish to rename
+Mostly works with files downloaded with opera mini
+"""
+
+# Get the current working directory
+path = os.getcwd()
+FileNames = os.listdir(path)
+
+#Renames files in the working directory containing %20 with Spaces
+def main():
+    for fileName in FileNames:
+        os.rename(fileName, fileName.replace("%20"," "))
+
+#Calls the main function
+main()
